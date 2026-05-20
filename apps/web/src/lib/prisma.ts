9a6746953c97200b9e70af3907b955d10beb652d
Bear <bearrambo9@gmail.com>
2026-05-20 14:22:04 -0400

    Get started setting up prisma and authjs. Initialized prisma schemas and made the client

---
+++ apps/web/src/lib/prisma.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "../../prisma/src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+let prismaInstance: PrismaClient;
+
+if (globalForPrisma.prisma) {
+  prismaInstance = globalForPrisma.prisma;
+} else {
+  const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  const adapter = new PrismaPg(pool);
+  prismaInstance = new PrismaClient({ adapter });
+
+  if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prismaInstance;
+  }
+}
+
+export const prisma = prismaInstance;
